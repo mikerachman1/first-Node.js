3e0e26b19e7b7a70060e613c39146fad63bc6754
Mike Rachman <mikerachman1@gmail.com>
2023-02-17 17:11:38 -0500

    express installed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('hello world!')
+})
+
+app.listen(PORT, () => {
+  console.log('App listening on port ' + PORT);
+})
