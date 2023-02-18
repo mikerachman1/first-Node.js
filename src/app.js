6e9be08caf4a4072fcb8b5e367141b307feef7da
Mike Rachman <mikerachman1@gmail.com>
2023-02-18 07:59:06 -0500

    added data and get request to return data

--- src/app.js
+++ src/app.js
@@ -2,13 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const customers = [
+  {
+    "name": "Caleb",
+    "industry": "music"
+  },
+  {
+    "name": "John",
+    "industry": "networking"
+  },
+  {
+    "name": "Sal",
+    "industry": "sports medicine"
+  }
+];
 
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.send('Welcome!')
+})
+
+app.get('/api/customers', (req, res) => {
+  res.send({"customers": customers})
 });
 
 app.post('/', (req, res) => {
-  res.send('This is a post request');
+  res.send('This is a post request!!!');
 });
 
 app.listen(PORT, () => {
